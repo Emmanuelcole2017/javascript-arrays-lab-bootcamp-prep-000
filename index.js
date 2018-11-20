3f59680211ec6122a2552136c8491e62efa655ca
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-20 22:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,31 @@ function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens
+}
 // Add your functions and code here
